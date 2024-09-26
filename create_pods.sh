25586d7ea3c4af81bf2e2e14216390d18ed55599
TUAN HOANG TRONG <tmhoangt@us.ibm.com>
2024-09-26 10:20:13 -0400

    feat: add script to create N pods

---
+++ create_pods.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# IMPORTANT: use 01-based pod id as this ID is used in calculating the offset of file content to process
+# RUN 1 pod: ./run.sh 01
+
+# run 11 pods from 01 to 11
+export NUM_PODS=11
+for id in $(seq -f "%02g" 1 ${NUM_PODS})
+do
+    echo $id
+    export id=${id}; shift
+    export POD_YAML=vllm-pod.yaml.${id}
+    envsubst < vllm-pod.yaml > ${POD_YAML}
+
+    oc apply -f ${POD_YAML}
+done
+
+for id in $(seq -f "%02g" 1 11)
+do
+    export POD_NAME="gen-pod-${id}"
+    ./wait_pod.sh  ${POD_NAME}
+done
