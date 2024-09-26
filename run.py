9e70f46799f2890159e22ff4a6b528cb1fa166fc
TUAN HOANG TRONG <tmhoangt@us.ibm.com>
2024-09-26 14:05:29 -0400

    fix typo

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import subprocess
+import os
 
 NUM_PODS = os.environ.get("NUM_PODS", 11)
 POD_PREFIX = os.environ.get("POD_PREFIX", "gen-pod")
 POD_WORKDIR = "/app"
 
 def run_in_pod(id=1):
-    podname = f"${POD_PREFIX}-{str(id).zfill(2)}"
+    podname = f"{POD_PREFIX}-{str(id).zfill(2)}"
     if 0:
         cmd = "apt-get update "
         cmd = f"kubectl exec {podname} -- {cmd}"
@@ -21,7 +22,7 @@ def run_in_pod(id=1):
 
     fnames = [
         "raga-gen.tar",
-        "run_in_image.sh 2>&1 | tee log_output.txt", # put the end
+        "run_in_image.sh", # put the end
     ]
     # copy new run code
     cmd = f"tar -cvf raga-gen.tar raga-gen"
@@ -34,14 +35,13 @@ def run_in_pod(id=1):
     for fname in fnames:
         tmux = "tmux send-keys -t 0 "
         if fname.endswith(".sh"):
-            cmd = f""" bash -c "chmod +x {POD_WORKDIR}/{fname} &&  cd {POD_WORKDIR} && {tmux}  'C-m sh ./{fname}' C-m;"  """
+            cmd = f""" bash -c "chmod +x {POD_WORKDIR}/{fname} &&  cd {POD_WORKDIR} && {tmux}  'sh ./{fname} 2>&1 | tee log_output.txt' C-m;"  """
         elif fname.endswith(".tar"):
             cmd = f"""  bash -c "cd {POD_WORKDIR} && tar -xvf {POD_WORKDIR}/{fname}" """
         elif fname.startswith("datalake"):
             cmd = f"pip install {POD_WORKDIR}/{fname}"
         cmd = f"kubectl exec {podname} -- {cmd}"
         print(cmd)
-        import subprocess
         result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output = result.stdout.decode("utf-8")
 
